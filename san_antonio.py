569da28c298022928b4958b6651d12185a6e572a
globalfranck <50407517+globalfranck@users.noreply.github.com>
2021-02-03 15:27:55 +0100

    pseudocode

--- san_antonio.py
+++ san_antonio.py
@@ -1,3 +1,11 @@
+# Show random quote
+
+# If user_answer is ‘B’: 
+# - leave the program
+
+# Else : 
+# - show another quote
+
 quotes = [
     "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes ! ",
     "On doit pouvoir choisir entre s'écouter parler et se faire entendre"
