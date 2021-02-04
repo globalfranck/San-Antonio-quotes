52dfcc8c29f5e8e979a97e13a7120706dc30bdd3
globalfranck <50407517+globalfranck@users.noreply.github.com>
2021-02-04 16:14:18 +0100

    Reorganizing the code

--- san_antonio.py
+++ san_antonio.py
@@ -3,6 +3,22 @@ import random
 # Import the json library to work with json external files
 import json
 
+# reading values from an external file
+def read_values_from_json(file, key):
+    # create an empty list
+    values = []
+
+    # open a json file with my objects
+    with open(file) as f:
+    # load all the data contained in this file
+        data = json.load(f)
+    # add each entry into my created list
+    for entry in data : 
+        values.append(entry[key])
+
+    # return my completed list
+    return values
+
 # function to obtain a random item from a list
 def get_random_item(my_list):
     # get a random number from the random library
@@ -18,29 +34,19 @@ def capitalize(words):
 
 # function to create a message which is comprised of the characters name and its quote
 def message(character, quote):
-    """capitalize(character)
-    capitalize(quote)"""
     return f"{character.capitalize()} a dit : {quote.capitalize()}"
 
-# First interaction with the user : launching or not the loop
-def read_values_from_json():
-    # create an empty list
-    values = []
+def get_random_quote():
+    all_values = read_values_from_json('quotes.json', 'quote')
+    return get_random_item(all_values)
 
-    # open a json file with my objects
-    with open("characters.json") as f:
-    # load all the data contained in this file
-        data = json.load(f)
-    # add each entry into my created list
-    for entry in data : 
-        values.append(entry["character"])
-
-    # return my completed list
-    return values
+def get_random_character():
+    all_values = read_values_from_json('characters.json', 'character')
+    return get_random_item(all_values)
 
 user_answer = input("Tapez entrée pour connaître une autre citation ou B pour quitter le programme.")
 
 # If user enters B => end the program, otherwise launch the program and show a random quote
 while user_answer != "B" :
-    print( message( get_random_item(characters), get_random_item(quotes)) )
+    print( message( get_random_character(), get_random_quote()) )
     user_answer = input("Tapez entrée pour connaître une autre citation ou B pour quitter le programme.")
